cdecb03379465ebfc84ec4860387c981d532bcd3
unknown <khalidhasanmeskat@gmail.com>
2026-03-18 12:47:35 +0600

    update button and more

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Country.css";
 
 const Country = ({ country }) => {
-  console.log(country.flags.flags.alt);
+  const [visited, setVisited] = useState(false);
+  // console.log(country);
+  const handleVisited = () => {
+    // system1
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    // system2
+    // setVisited(visited ? false : true);
+    // system3
+    setVisited(!visited);
+    
+  };
   return (
-    <div>
+    <div className={`country ${visited ? "country-visited" : ""}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-      <h3>Name : {country.name.common}</h3>
+
+      <h3>Name: {country.name.common}</h3>
+      <p>Population: {country.population.population}</p>
+      <p>Capital: {country.capital.capital}</p>
+      <p>Region: {country.region.region}</p>
+      <p>
+        Area: {country.area.area}{" "}
+        {country.area.area > 30000 ? "Big Country" : "Small Country"}
+      </p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,14 +1,19 @@
 import React, { use } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
-  
+
   return (
     <div>
-      <h2>In the countries: {countries.length}</h2>
-      {countries.map(country => <Country country={country}></Country>)}
+      <h1>Countries Information Are: {countries.length}</h1>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
